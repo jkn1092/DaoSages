f3868f76a2fb50f43241141e819392f0621b5dc8
Jeremy N <jkn1092@gmail.com>
2023-03-27 00:46:45 +0200

    add main unit testing

--- backend/contracts/DAOSage.sol
+++ backend/contracts/DAOSage.sol
@@ -10,13 +10,12 @@ contract DAOSage {
         address owner;
         address escrowWallet;
         bool validated;
-        uint8 score;
     }
 
     address public admin;
     uint public projectCount;
     mapping(uint => Project) public projects;
-    mapping(uint => mapping(address => bool)) projectsVotes;
+    mapping(uint => mapping(address => uint8)) public projectsVotes;
     mapping(address => bool) public wise;
     mapping(address => bool) public brainers;
 
@@ -34,6 +33,7 @@ contract DAOSage {
 
     constructor() {
         admin = msg.sender;
+        wise[msg.sender] = true;
     }
 
     function submitProject(string memory _name) payable public {
@@ -51,18 +51,10 @@ contract DAOSage {
     }
 
     function vote(uint256 _index, uint8 _grade) public {
-        require(_index >= 0 && _index < projectCount, "Invalid project index.");
+        require(_index >= 0 && _index <= projectCount, "Invalid project index.");
         require(_grade >= 0 && _grade <= 10, "Grade must be between 1 and 10.");
-        require(!projectsVotes[_index][msg.sender], "Already voted.");
 
-        uint8 coefficient = 1;
-        if( wise[msg.sender] )
-            coefficient = 3;
-        else if ( brainers[msg.sender] )
-            coefficient = 2;
-
-        projectsVotes[_index][msg.sender] = true;
-        projects[_index].score = (projects[_index].score + (_grade * coefficient)) / (coefficient + 1);
+        projectsVotes[_index][msg.sender] = _grade;
 
         emit VoteSubmitted(msg.sender, _index, _grade);
     }
@@ -72,12 +64,13 @@ contract DAOSage {
     }
 
     function getProject(uint256 _index) public view returns (address, string memory) {
-        require(_index >= 0 && _index < projectCount, "Invalid project index.");
+        require(_index > 0 && _index <= projectCount, "Invalid project index.");
         Project memory project = projects[_index];
         return (project.owner, project.name);
     }
 
     function validateProject(uint _id) public {
+        require(_id > 0 && _id <= projectCount, "Invalid project index.");
         require(!projects[_id].validated, "Proposal has already been executed.");
         require(wise[msg.sender], "Validation done only by wise.");
         projects[_id].validated = true;
--- backend/test/DAOSage.js
+++ backend/test/DAOSage.js
@@ -1,11 +1,13 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const {address} = require("hardhat/internal/core/config/config-validation");
 
 describe("DAOSage Contract", function () {
-    let dao;
+    let dao, admin, wise, brainer, finder
 
     // Deploy the contract before each test
     beforeEach(async function () {
+        [admin, wise, brainer, finder] = await ethers.getSigners();
         const DAOSage = await ethers.getContractFactory("DAOSage");
         dao = await DAOSage.deploy();
         await dao.deployed();
@@ -19,9 +21,62 @@ describe("DAOSage Contract", function () {
             const project = await dao.projects(1);
             expect(project.id).to.equal(1);
             expect(project.name).to.equal(projectName);
-            expect(project.owner).to.equal(await ethers.provider.getSigner(0).getAddress());
+            expect(project.owner).to.equal(admin.address);
             expect(project.validated).to.equal(false);
-            expect(project.score).to.equal(0);
+            expect(ethers.utils.isAddress(project.escrowWallet)).to.be.true;
+        });
+
+        it("should revert if project name is empty", async function () {
+            await expect(dao.submitProject("")).to.be.revertedWith("Project name must not be empty.");
+        });
+    });
+
+    // Define a test case for the vote function
+    describe("vote", function () {
+        it("should add a new vote", async function () {
+            const projectName = "My Project";
+            await dao.submitProject(projectName);
+            await dao.vote(1, 8);
+            const projectVote = await dao.projectsVotes(1, admin.address);
+            expect(projectVote).to.equal(8);
+        });
+
+        it("should revert with invalid project index", async function () {
+            await expect(dao.vote(1, 8)).to.be.revertedWith("Invalid project index.");
+        });
+
+        it("should revert with invalid grade", async function () {
+            const projectName = "My Project";
+            await dao.submitProject(projectName);
+            await expect(dao.vote(1, 12)).to.be.revertedWith("Grade must be between 1 and 10.");
+        });
+    });
+
+    // Define a test case for the validateProject function
+    describe("validateProject", function () {
+        it("should validate a project", async function () {
+            const projectName = "My Project";
+            await dao.submitProject(projectName);
+            await dao.validateProject(1);
+            const project = await dao.projects(1);
+            expect(project.validated).to.equal(true);
+        });
+
+        it("should revert with invalid project index", async function () {
+            await expect(dao.validateProject(1)).to.be.revertedWith("Invalid project index.");
+        });
+
+        it("should revert with proposal already validated", async function () {
+            const projectName = "My Project";
+            await dao.submitProject(projectName);
+            await dao.validateProject(1);
+            await expect(dao.validateProject(1)).to.be.revertedWith("Proposal has already been executed.");
+        });
+
+        it("should revert with not wise user", async function () {
+            const projectName = "My Project";
+            await dao.submitProject(projectName);
+            await expect(dao.connect(brainer).validateProject(1)).to.be.revertedWith("Validation done only by wise.");
         });
     });
 });
