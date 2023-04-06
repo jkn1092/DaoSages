b1fc6fa109398dc04eb71a30ed48595840afc321
Jeremy N <jkn1092@gmail.com>
2023-04-06 14:43:18 +0200

    Add NatSpec

--- backend/contracts/DAOSage.sol
+++ backend/contracts/DAOSage.sol
@@ -7,55 +7,80 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+/**
+ * @title DAOSage
+ * @notice This contract for a DAO with ERC721 tokens that represent different roles within the organization.
+ * @dev Members with these roles (finders, brainers, wisemen) can submit and audit projects,
+ * and a total score is calculated for each project based on the grades it receives.
+ * @author Jeremy Narrainasamy
+ */
 contract DAOSage is Ownable, ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    /// @dev Struct to define the tokenIds a participant has for each role.
     struct Participant {
-        uint tokenFinder;
-        uint tokenBrainer;
-        uint tokenWisemen;
+        uint tokenFinder;                       // ERC721 token ID for "finder" role
+        uint tokenBrainer;                      // ERC721 token ID for "brainer" role
+        uint tokenWisemen;                      // ERC721 token ID for "wisemen" role
     }
 
+    /// @dev Struct to define an audit of a project.
     struct Audit {
-        bool audited;
-        uint8 grade;
+        bool audited;                           // Indicates if the project has been audited
+        uint8 grade;                            // The grade (0-10) given to the project
     }
 
+    /// @dev Struct to define a project.
     struct Project {
-        string name;
-        address owner;
-        mapping(address => Audit) audits;
-        uint totalScore;
-        uint nbScore;
+        string name;                            // The name of the project
+        address owner;                          // The address of the project owner
+        mapping(address => Audit) audits;       // Mapping of auditor addresses to their audit
+        uint totalScore;                        // The total score of the project
+        uint nbScore;                           // The number of auditors who have audited the project
     }
 
+    // The number of registered addresses
     uint public nbRegisteredAddress;
+
+    // Mapping of participant addresses to their roles
     mapping(address => Participant) public participants;
+
+    // Array of all projects
     Project[] public projects;
 
-    // Define an event to log when a new project is submitted
+    /// @dev Define an event to log when a new project is submitted
     event ProjectSubmitted(address owner, uint id, string name);
 
-    // Define an event to log when an audit is submitted
+    /// @dev Define an event to log when an audit is submitted
     event AuditSubmitted(address indexed voter, uint indexed id, uint8 grade);
 
+    /// @dev Modifier that restricts a function to finders and wisemen.
     modifier onlyFinders() {
         require( _exists(participants[msg.sender].tokenFinder) || _exists(participants[msg.sender].tokenWisemen) ,
             "Not finder or wisemen");
         _;
     }
 
+    /// @dev Modifier that restricts a function to brainers and wisemen.
     modifier onlyBrainers() {
         require( _exists(participants[msg.sender].tokenBrainer) || _exists(participants[msg.sender].tokenWisemen),
             "Not brainer or wisemen");
         _;
     }
 
+    /**
+     * @dev Constructor that mints a "wisemen" token for the contract owner.
+     * Initializes the ERC721 contract with the name "DaoSages" and the symbol "DS".
+     */
     constructor() ERC721("DaoSages", "DS") {
         mintWisemen(msg.sender);
     }
 
+    /**
+     * @dev Function to mint a Finder token to the specified address and assigns the token URI
+     * @param _to The address of the user to whom the token is to be minted
+     */
     function mintFinder(address _to) external payable {
         require(!_exists(participants[_to].tokenFinder), 'Already minted');
         require(msg.value >= 0.01 ether, 'insufficient eth');
@@ -68,6 +93,10 @@ contract DAOSage is Ownable, ERC721URIStorage {
         nbRegisteredAddress++;
     }
 
+    /**
+     * @dev Function to mint a Brainer token to the specified address and assigns the token URI
+     * @param _to The address of the user to whom the token is to be minted
+     */
     function mintBrainer(address _to) external payable{
         require(!_exists(participants[_to].tokenBrainer), 'Already minted');
         require(msg.value >= 0.02 ether, 'insufficient eth');
@@ -80,6 +109,10 @@ contract DAOSage is Ownable, ERC721URIStorage {
         nbRegisteredAddress++;
     }
 
+    /**
+     * @dev Function to mint a Wisemen token to the specified address and assigns the token URI
+     * @param _to The address of the user to whom the token is to be minted
+     */
     function mintWisemen(address _to) onlyOwner public {
         require(!_exists(participants[_to].tokenWisemen), 'Already minted');
 
@@ -91,6 +124,10 @@ contract DAOSage is Ownable, ERC721URIStorage {
         nbRegisteredAddress++;
     }
 
+    /**
+     * @dev Returns whether the calling address is a finder, a brainer, and/or a wiseman.
+     * @return Three booleans, indicating whether the calling address is a finder, a brainer, and/or a wiseman.
+     */
     function getRoles() public view returns (bool isFinder, bool isBrainer, bool isWise) {
         uint tokenFinder = participants[msg.sender].tokenFinder;
         uint tokenBrainer = participants[msg.sender].tokenBrainer;
@@ -106,21 +143,39 @@ contract DAOSage is Ownable, ERC721URIStorage {
             isWise = true;
     }
 
+    /**
+     * @dev Returns the URI of the calling address's finder token.
+     * @return string containing the URI of a token
+     */
     function tokenFinderURI() public view returns(string memory) {
         require(_exists(participants[msg.sender].tokenFinder), 'Finder not mint');
         return tokenURI(participants[msg.sender].tokenFinder);
     }
 
+    /**
+     * @dev Returns the URI of the calling address's brainer token.
+     * @return string containing the URI of a token
+     */
     function tokenBrainerURI() public view returns(string memory) {
         require(_exists(participants[msg.sender].tokenBrainer), 'Brainer not mint');
         return tokenURI(participants[msg.sender].tokenBrainer);
     }
 
+    /**
+     * @dev Returns the URI of the calling address's wisemen token.
+     * @return string containing the URI of a token
+     */
     function tokenWiseURI() public view returns(string memory) {
         require(_exists(participants[msg.sender].tokenWisemen), 'Wise not mint');
         return tokenURI(participants[msg.sender].tokenWisemen);
     }
 
+    /**
+     * @dev Function to submit a new project.
+     * Only participants with the "Finder" or "Wisemen" tokens can execute this function.
+     * Emits a ProjectSubmitted event.
+     * @param _name The name of the project to be submitted.
+     */
     function submitProject(string memory _name) public onlyFinders {
         require(bytes(_name).length > 0, "Project name must not be empty.");
 
@@ -131,6 +186,13 @@ contract DAOSage is Ownable, ERC721URIStorage {
         emit ProjectSubmitted(msg.sender, projects.length-1, _name);
     }
 
+    /**
+     * @dev Function to audit a project.
+     * Only participants with the "Brainer" or "Wisemen" tokens can execute this function.
+     * Emits a AuditSubmitted event.
+     * @param _index The id of the project to be audited.
+     * @param _grade The grade of the project to be audited.
+     */
     function auditProject(uint256 _index, uint8 _grade) public onlyBrainers {
         require(_index >= 0 && _index < projects.length, "Invalid project index.");
         require(_grade >= 0 && _grade <= 10, "Grade must be between 1 and 10.");
@@ -153,6 +215,11 @@ contract DAOSage is Ownable, ERC721URIStorage {
         emit AuditSubmitted(msg.sender, _index, _grade);
     }
 
+    /**
+     * @dev Function to get audit of a project.
+     * @param _index The id of the project.
+     * @return uint containing the audit of the project
+     */
     function getAudit(uint256 _index) public view returns(uint audit) {
         require(_index >= 0 && _index < projects.length, "Invalid project index.");
 
@@ -160,6 +227,11 @@ contract DAOSage is Ownable, ERC721URIStorage {
             audit = projects[_index].totalScore / projects[_index].nbScore;
     }
 
+    /**
+     * @dev Function to get weight of a voter based on the roles minted. Used by the DAOSageGovernance.
+     * @param _voter The address of the voter.
+     * @return uint containing the weight of the voter
+     */
     function getVoteWeight(address _voter) public view returns(uint8 weight){
         if( _exists(participants[_voter].tokenWisemen) )
             weight = 4;
@@ -169,6 +241,11 @@ contract DAOSage is Ownable, ERC721URIStorage {
             weight = 1;
     }
 
+    /**
+     * @dev Function to check if an address is a participant. Used by the DAOSageGovernance.
+     * @param _participant The address of the user.
+     * @return boolean whether the user is a participant
+     */
     function isParticipant(address _participant) public view returns(bool){
         return(_exists(participants[_participant].tokenFinder) || _exists(participants[_participant].tokenBrainer) ||
         _exists(participants[_participant].tokenWisemen));
--- backend/contracts/DAOSageGovernance.sol
+++ backend/contracts/DAOSageGovernance.sol
@@ -5,43 +5,68 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./DAOSage.sol";
 
+/**
+ * @title DAOSageGovernance
+ * @notice This contract is used for managing proposals and votes in a decentralized autonomous organization
+ * @dev It allows participants to submit proposals, vote on them, and withdraw their votes.
+ * @author Jeremy Narrainasamy
+ */
 contract DAOSageGovernance is Ownable{
 
+    /// @dev Structs to define a Vote
     struct Vote {
-        bool voted;
-        uint8 weight;
+        bool voted;                     // whether the participant has voted or not
+        uint8 weight;                   // the weight of the vote (determined by the DAOSage)
     }
 
+    /// @dev Structs to define a Proposal
     struct Proposal {
-        string name;
-        string description;
-        address owner;
-        bool validated;
-        uint voteCount;
+        string name;                    // the name of the proposal
+        string description;             // the description of the proposal
+        address owner;                  // the address of the participant who submitted the proposal
+        bool validated;                 // the proposal has been validated or not
+        uint voteCount;                 // the total weight of the votes received
     }
 
+    // Instance of DAOSage contract
     DAOSage daoSage;
+
+    // Array to store all proposals
     Proposal[] public proposals;
+
+    // Mapping to store the votes of each participant for each proposal
     mapping(uint => mapping(address => Vote)) proposalsVoted;
 
-    // Define an event to log when a new proposal is submitted
+    /// @dev Define an event to log when a new proposal is submitted
     event ProposalSubmitted(address owner, uint id, string name, string description);
 
-    // Define an event to log when a vote is submitted
+    /// @dev Define an event to log when a vote is submitted or withdrawn
     event VoteSubmitted(address indexed voter, uint indexed id, bool voted);
 
-    // Define an event to log when a proposal is validated
+    /// @dev Define an event to log when a proposal is validated
     event ProposalValidated(uint id);
 
+    /// @dev Modifier to restrict access to only DAOSage participants
     modifier onlyParticipants() {
         require( daoSage.isParticipant(msg.sender) ,"Not participant");
         _;
     }
 
+    /**
+     * @dev Constructor to set the instance of the DAOSage contract.
+     * @param _daoAddress The address of the DAOSage contract.
+     */
     constructor(address _daoAddress){
         daoSage = DAOSage(_daoAddress);
     }
 
+    /**
+     * @dev Function to submit a new proposal.
+     * Only participants with a token can execute this function.
+     * Emits a ProposalSubmitted event.
+     * @param _name The name of the proposal.
+     * @param _desc The description of the proposal.
+     */
     function submitProposal(string calldata _name, string calldata _desc) public onlyParticipants {
         require(bytes(_name).length > 0 && bytes(_desc).length > 0,
             "Proposal name and description must not be empty.");
@@ -55,12 +80,24 @@ contract DAOSageGovernance is Ownable{
         emit ProposalSubmitted(msg.sender, proposals.length - 1, _name, _desc);
     }
 
+    /**
+     * @dev Function to get a specific proposal.
+     * @param _id The ID of the proposal.
+     * @return The proposal details.
+     */
     function getProposal(uint _id) public view returns(Proposal memory) {
         require(_id < proposals.length, 'Proposal not found');
 
         return proposals[_id];
     }
 
+    /**
+     * @dev Function to submit a vote for a proposal.
+     * Only participants with a token can execute this function.
+     * Emits a VoteSubmitted event.
+     * Emits a ProposalValidated event if proposal is validated.
+     * @param _id The ID of the proposal.
+     */
     function submitVote(uint _id) public onlyParticipants {
         require(_id < proposals.length, 'Proposal not found');
         require(!proposals[_id].validated, 'Already validated');
@@ -80,6 +117,12 @@ contract DAOSageGovernance is Ownable{
         }
     }
 
+    /**
+     * @dev Function to withdraw a vote for a proposal.
+     * Only participants with a token can execute this function.
+     * Emits a VoteSubmitted event.
+     * @param _id The ID of the proposal.
+     */
     function withdrawVote(uint _id) public onlyParticipants {
         require(_id < proposals.length, 'Proposal not found');
         require(proposalsVoted[_id][msg.sender].voted, 'Already withdrawn');
