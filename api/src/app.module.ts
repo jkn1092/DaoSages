75fc03bdc84d035085d939ab1ad172e1415dbeb9
Jeremy N <jkn1092@gmail.com>
2023-04-07 12:06:33 +0200

    config mongodb

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -7,13 +7,13 @@ import { AppService } from './app.service';
 import { ProjectsModule } from './projects/projects.module';
 import {join} from "path";
 
-const url = process.env.MONGO_URL || '127.0.0.1';
+const url = process.env.MONGO_URL || '127.0.0.1:27017';
 const user = process.env.MONGO_USER || '';
 const password = process.env.MONGO_PASSWORD || '';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(`mongodb://${user}:${password}@${url}:27017`, {
+    MongooseModule.forRoot(`mongodb+srv://${user}:${password}@${url}`, {
       dbName: 'dao_sages',
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
