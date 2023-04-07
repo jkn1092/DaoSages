b580ce499e324a17179dbda772e11f872494db2e
Jeremy N <jkn1092@gmail.com>
2023-04-07 13:30:12 +0200

    Update apollo-client.js

--- client/apollo-client.js
+++ client/apollo-client.js
@@ -2,7 +2,7 @@ import {ApolloClient, ApolloLink, from, HttpLink, InMemoryCache} from "@apollo/c
 import {onError} from "@apollo/client/link/error";
 
 const httpLink = new HttpLink({
-    uri: 'http://daosages.herokuapp.com/graphql,
+    uri: 'http://daosages.herokuapp.com/graphql',
 });
 
 const activityMiddleware = new ApolloLink((operation, forward) => {
