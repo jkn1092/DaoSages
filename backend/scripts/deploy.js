422956cffc0899711178a1569ec6cec5ce6bfa00
Jeremy N <jkn1092@gmail.com>
2023-04-09 00:10:13 +0200

    Update deploy.js

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -7,6 +7,7 @@
 const hre = require("hardhat");
 
 async function main() {
+  const signer = await hre.ethers.provider.getSigner();
   const DaoSage = await hre.ethers.getContractFactory("DAOSage");
   const daoSage = await DaoSage.deploy();
   await daoSage.deployed();
@@ -21,6 +22,15 @@ async function main() {
   console.log(
       `Contract Governance deployed to ${daoGovernance.address}`
   );
+
+  await daoSage.connect(signer).submitProject("Script deployment project");
+  const project = await daoSage.projects(0);
+  console.log(project);
+
+  await daoGovernance.connect(signer).submitProposal("Script deployment proposal",
+      "This proposal has been added through the script during deployment.");
+  const proposal = await daoGovernance.getProposal(0);
+  console.log(proposal);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
