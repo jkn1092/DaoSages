5cb5f6f931fcf6e1efab86f1c8d42c4ecf02772b
Jeremy N <jkn1092@gmail.com>
2023-04-10 19:14:50 +0200

    Update data.js

--- backend/scripts/data.js
+++ backend/scripts/data.js
@@ -2,14 +2,18 @@ const hre = require("hardhat");
 
 async function main() {
     const DaoSage = await hre.ethers.getContractFactory("DAOSage");
-    const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    const contractAddress = "0xf6446e926Fa3aa2282eC9c8D72C06D459FBF80F8";
 
     const DAOSageGovernance = await hre.ethers.getContractFactory("DAOSageGovernance");
-    const governanceAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+    const governanceAddress = "0xca561fC618ad204734aD6B911DE1AE80c4a6D048";
+
+    const address = hre.ethers.utils.getAddress("0x0f49abe5eed491d14fDD1f5842bAce069aB3A2b2");
 
     const signer = await hre.ethers.provider.getSigner();
     const contract = await DaoSage.attach(contractAddress);
     const governance = await DAOSageGovernance.attach(governanceAddress);
+
+    //await contract.connect(signer).mintWisemen(address);
 
     const value = await contract.getAudit(0);
     console.log("Stored value:", value);
@@ -18,10 +22,6 @@ async function main() {
 
     const newValue = await contract.getAudit(0);
     console.log("Stored value:", newValue);
-
-    await governance.connect(signer).submitProposal("Script creation", "This proposal has been added through a script.");
-    const proposal = await governance.getProposal(0);
-    console.log(proposal);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
